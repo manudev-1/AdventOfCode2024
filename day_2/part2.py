3d0082435befd3a95897e2167e3647bf18c100de
manudev-1 <manuelebarone186@gmail.com>
2024-12-02 20:32:43 +0100

    day 2 done

--- day_2/part2.py
+++ day_2/part2.py
@@ -3,17 +3,28 @@ with open("/home/manu/Desktop/AdventOfCode2024/day_2/input.txt") as f:
 
 k = 0
 
-for line in lines:
-    splitted = line.split(" ")
-    levels = [int(x) for x in splitted] 
-
+def is_safe(levels):
     is_increasing = all(levels[i] <= levels[i + 1] for i in range(len(levels) - 1))
     is_decreasing = all(levels[i] >= levels[i + 1] for i in range(len(levels) - 1))
 
     if is_increasing or is_decreasing:
         differences = [abs(levels[i] - levels[i + 1]) for i in range(len(levels) - 1)]
-        
-        if all(1 <= diff <= 3 for diff in differences):
+        return all(1 <= diff <= 3 for diff in differences)
+    return False
+
+
+for line in lines:
+    splitted = line.split(" ")
+    levels = [int(x) for x in splitted] 
+
+    if is_safe(levels):
+        k += 1
+        continue
+
+    for i in range(len(levels)):
+        modified_lvls = levels[:i] + levels[i+1:]
+        if is_safe(modified_lvls):
             k += 1
+            break
 
 print(k)
